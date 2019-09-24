c6dd458e7b369b59b745b1a050b91b03f09740e4
jvme <juanv.mari@gmail.com>
2019-09-24 17:33:41 +0200

    Creación de la web de tarot

---
+++ src/main/java/com/tarotyastrologia/TarotyastrologiaApplication.java
@@ -0,0 +1,13 @@
+package com.tarotyastrologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarotyastrologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarotyastrologiaApplication.class, args);
+	}
+
+}
