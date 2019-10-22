3d89f9ba24f96f1f014a4da97632292728cbe344
jvme <juanv.mari@gmail.com>
2019-10-23 00:48:18 +0200

    Grid pruebas

---
+++ src/main/resources/static/js/aspectsgrid.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+(function(aspects) {
+	aspects.SEXTIL = 1;
+	aspects.CUADRATURA = 2;
+	aspects.createGrid = function(idGrid) {
+		this.idGrid = idGrid;
+		return this;
+	};
+	
+}(window.aspects = window.aspects || {}));
